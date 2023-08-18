0794434329dcdab432d0d2890ae16cbf4f3b7f49
Asttear <asttear@sina.cn>
2023-08-19 01:22:50 +0800

    WPF：更新关于页面

--- HrbustDoggy.Wpf/Views/MainWindow.xaml.cs
+++ HrbustDoggy.Wpf/Views/MainWindow.xaml.cs
@@ -4,6 +4,7 @@ using HrbustDoggy.Wpf.ViewModels;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.ComponentModel;
+using System.Diagnostics;
 using System.Reflection;
 using System.Windows;
 
@@ -61,8 +62,19 @@ public partial class MainWindow : Window
     private void About_Click(object sender, RoutedEventArgs e)
     {
         string? version = Assembly.GetExecutingAssembly().GetName().Version?.ToString();
-        MessageBox.Show(this, $"哈尔滨理工大学教务在线客户端\n理工汪 V{version} 内部测试版\n\n©2023 幺零贰实验室，版权没有。",
-            "关于", MessageBoxButton.OK, MessageBoxImage.Information);
+        string message = $"""
+            哈尔滨理工大学教务在线客户端
+            理工汪 V{version} 内部测试版
+            
+            ©2023 幺零贰实验室，版权没有。
+            
+            点击“确定”以访问项目主页。
+            """;
+        MessageBoxResult result = MessageBox.Show(this, message, "关于", MessageBoxButton.OKCancel, MessageBoxImage.Information);
+        if (result == MessageBoxResult.OK)
+        {
+            Process.Start(new ProcessStartInfo("https://github.com/Asttear/HrbustDoggy") { UseShellExecute = true });
+        }
     }
 
     private void Exit_Click(object sender, RoutedEventArgs e)
